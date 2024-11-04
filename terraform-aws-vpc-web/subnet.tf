4741652502bb5b0035d5bcc9be863c54d3a9dd37
Ubuntu <ubuntu@ip-172-31-32-92.ec2.internal>
2024-11-04 22:19:35 +0000

    fix:correcting file name & using region variables

--- terraform-aws-vpc-web/subnet.tf
+++ terraform-aws-vpc-web/subnet.tf
@@ -1,12 +1,12 @@
 resource "aws_subnet" "subnet_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.1.0.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "${var.region}a"
 }
 
 
 resource "aws_subnet" "subnet_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.1.1.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone ="${var.region}b"
 }
