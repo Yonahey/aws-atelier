82316dd519ca50be193ed56fb482b4276f0653d1
Yonae <cherfi.ismael@gmail.com>
2024-11-17 23:00:34 +0000

    fix: formated files

--- terraform-aws-vpc-foreach/locals.tf
+++ terraform-aws-vpc-foreach/locals.tf
@@ -1,14 +1,14 @@
 locals {
   vpc_cidr = "10.1.0.0/16"
-  owner        = "ic"
-  tp           = "terraform-aws-vpc-dynamic"
+  owner    = "ic"
+  tp       = "terraform-aws-vpc-for_each"
   subnets = {
     "10.1.0.0/24" = "us-east-1a"
     "10.1.1.0/24" = "us-east-1b"
     "10.1.2.0/24" = "us-east-1c"
   }
   region = "us-east-1"
-  date = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
+  date   = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
 }
 
 locals {
--- terraform-aws-vpc-foreach/tableAssociation.tf
+++ terraform-aws-vpc-foreach/tableAssociation.tf
@@ -1,6 +1,6 @@
 resource "aws_route_table_association" "main" {
-  for_each  = aws_subnet.subnets
-  subnet_id = each.value.id
+  for_each       = aws_subnet.subnets
+  subnet_id      = each.value.id
   route_table_id = aws_route_table.main.id
 }
 # resource "aws_route_table_association" "table_association_b" {
