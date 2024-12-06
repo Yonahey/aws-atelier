35223a5fff422dd12725e4a090eedbb4e87268a0
Cherfi Ismaël <icherfi@apizr.fr>
2024-12-06 20:01:18 +0100

    feat:updating outputs

--- terraform-aws-vpc-registry/output.tf
+++ terraform-aws-vpc-registry/output.tf
@@ -1,12 +1,22 @@
-output "instance_id" {
+output "ec2module_instance_id" {
   value = module.ec2.instance_id
 }
 
-output "instance_private_ip" {
+output "ec2_module_instance_private_ip" {
   value = module.ec2.instance_private_ip
 }
 
-output "instance_private_dns" {
+output "ec2_module_instance_private_dns" {
   value = module.ec2.instance_private_dns
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_id" {
+
+  value = aws_subnet.main.id
+
 }
 
